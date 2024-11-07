8b8f8d4ec83a475ca0cc19612d6402df57bbfced
MURoyale <52778859+MURoyale@users.noreply.github.com>
2024-11-07 19:58:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import "./App.css";
-import LeftCont from "./leftCont/leftCont";
-import Container from "./Container/Container";
 import RightCont from "./RightCont/RightCont";
 
 function App() {
-  
   return (
     <>
-          <RightCont />   
+      <RightCont />
     </>
   );
 }
 
 export default App;
-
-
